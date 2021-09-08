e94203ce0d8dd63e327a5f84b31b848694de381b
prathipati supriya <supriyaprathipati9@gmail.com>
2021-09-08 13:03:19 +0530

    update

--- 4_Implementation/src/HalfwaveandFullwave.c
+++ 4_Implementation/src/HalfwaveandFullwave.c
@@ -80,9 +80,10 @@ int  operationofFullwave()
   printf("The Percentage regulation of fullwave is %f \n",Percentageregulation);
   return 1;
 }
-int Vdc(int vdc){
-  vdc=0;
-  if(vdc>0)
+int Vdc(int Vdc)
+{
+  Vdc=0;
+  if(Vdc>0)
   {
     return 0;
   }
